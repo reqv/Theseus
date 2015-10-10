3fe95b7ee85710a4d6dae07ff63aea59984cb92b
Wojciech Janeczek <wojciech.janeczek@protonmail.com>
2015-10-10 21:04:07 +0200

    Dodalem pajaka w poczatkowej wersji

--- Assets/Scripts/Monsters/Snake.cs
+++ Assets/Scripts/Monsters/Snake.cs
@@ -3,21 +3,18 @@ using System.Collections;
 
 public class Snake : Monster {
 
-    private Transform _targetToAttack;
-    private Rigidbody2D _snakeRig2D;
-	private Vector2 _axis,_collisionAxis,_freeDestination;	//zmienne odpowiedzialne za pamietanie kierunku poruszania i celu podróży obiektu
-	private float _personalMaxSpeed;		// podmiana dla _maxSpeed / ze wzgledu na dodawanie predkosci
-	
 	[Tooltip ("Prędkość dodawana przy spostrzeżeniu gracza.")]
 	[SerializeField]
 	private int _chaseAdditionalSpeed;
 
+	private float _personalMaxSpeed;		// podmiana dla _maxSpeed / ze wzgledu na dodawanie predkosci
+
 	override public void Start () {
 		base.Start();
 		_personalMaxSpeed = _maxSpeed;
-        _snakeRig2D = GetComponent<Rigidbody2D>();
+        _Rig2D = GetComponent<Rigidbody2D>();
         _axis = new Vector2(0, 0);
-        _freeDestination = new Vector2(_snakeRig2D.position.x, _snakeRig2D.position.y);
+        _freeDestination = new Vector2(_Rig2D.position.x, _Rig2D.position.y);
 	}
 
 	void OnCollisionEnter2D(Collision2D other)
@@ -31,40 +28,26 @@ public class Snake : Monster {
 	void OnCollisionStay2D(Collision2D other)
 	{
 		if(_collisionAxis.y < 0){
-			_freeDestination.y = _snakeRig2D.position.y + _patrolRange;
+			_freeDestination.y = _Rig2D.position.y + _patrolRange;
 		}
 		else{
-			_freeDestination.y = _snakeRig2D.position.y - _patrolRange;
+			_freeDestination.y = _Rig2D.position.y - _patrolRange;
 		}
 		
 		if(_collisionAxis.x < 0){
-			_freeDestination.x = _snakeRig2D.position.x + _patrolRange;
+			_freeDestination.x = _Rig2D.position.x + _patrolRange;
 		}
 		else{
-			_freeDestination.x = _snakeRig2D.position.x - _patrolRange;
+			_freeDestination.x = _Rig2D.position.x - _patrolRange;
 		}
 	}
 
-	void FixedUpdate () {
-		float distance = float.MaxValue;
-		if (GameObject.FindGameObjectWithTag ("Player") != null) {
-			_targetToAttack = GameObject.FindGameObjectWithTag("Player").transform;
-			distance = Vector2.Distance(transform.position,_targetToAttack.position);
-		}
-        if (distance <= _attackDistance)
-            Attack();
-        else if (distance <= _range)
-            Chase();
-        else
-            Walking();
-	}
-
-    void Attack()
+    public override void Attack()
     {
      //   Debug.Log("Attakck!! !! ");
     }
 
-    void Chase()
+	public override void Chase()
     {
 		if (_maxSpeed == _personalMaxSpeed)
 			_personalMaxSpeed += _chaseAdditionalSpeed;
@@ -77,28 +60,28 @@ public class Snake : Monster {
             _axis.y = -1;
         else
             _axis.y = 1;
-        _snakeRig2D.AddForce(_axis * _personalMaxSpeed);
+        _Rig2D.AddForce(_axis * _personalMaxSpeed);
     }
 
-    void Walking()
+	public override void Walking()
     {
 		if (_maxSpeed != _personalMaxSpeed)
 			_personalMaxSpeed = _maxSpeed;
-        if (Vector2.Distance(_snakeRig2D.position, _freeDestination) >= 1)
+        if (Vector2.Distance(_Rig2D.position, _freeDestination) >= 1)
         {
-            if (_snakeRig2D.position.x - _freeDestination.x < 0)
+            if (_Rig2D.position.x - _freeDestination.x < 0)
                 _axis.x = 1;
             else
                 _axis.x = -1;
-            if (_snakeRig2D.position.y - _freeDestination.y < 0)
+            if (_Rig2D.position.y - _freeDestination.y < 0)
                 _axis.y = 1;
             else
                 _axis.y = -1;
-            _snakeRig2D.AddForce(_axis * _personalMaxSpeed);
+            _Rig2D.AddForce(_axis * _personalMaxSpeed);
         }
             else
         {
-			_freeDestination = new Vector2(_snakeRig2D.position.x + getRandomNumber(-_patrolRange,_patrolRange),_snakeRig2D.position.y +  getRandomNumber(-_patrolRange,_patrolRange));
+			_freeDestination = new Vector2(_Rig2D.position.x + getRandomNumber(-_patrolRange,_patrolRange),_Rig2D.position.y +  getRandomNumber(-_patrolRange,_patrolRange));
         }
     }
 }
