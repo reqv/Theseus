687aa2f2d8b0eb3a6ee282d0c724c0584a731ec9
Lukasz Rabiec <lukaszrabiec@gmail.com>
2015-11-08 20:05:22 +0100

    Poprawka na Board

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -24,14 +24,14 @@ public class Board
     {
 
         /// <summary>
-        /// Współrzędna X
+        /// Współrzędna X - Kolumna
         /// </summary>
         public int X
         {
             get; set;
         }
         /// <summary>
-        /// Współrzędna Y
+        /// Współrzędna Y - Wiersz
         /// </summary>
         public int Y
         {
@@ -41,8 +41,8 @@ public class Board
         /// <summary>
         /// Konstruktor klasy Coords przyjmujący dwie wartości.
         /// </summary>
-        /// <param name="x">Współrzędna X</param>
-        /// <param name="y">Współrzędna Y</param>
+        /// <param name="x">Współrzędna X - Kolumna</param>
+        /// <param name="y">Współrzędna Y - Wiersz</param>
         public Coords(int x, int y)
         {
             X = x;
@@ -51,70 +51,43 @@ public class Board
     }
 
     /// <summary>
-    /// Enumerator odpowiadający za rodzaje pokoi (komórek w tabeli).
-    /// </summary>
-    private enum Cell
-    {
-        /// <summary>
-        /// Brak pokoju
-        /// </summary>
-        Empty = 0,
-        /// <summary>
-        /// Zwykły pokój
-        /// </summary>
-        Common = 1,
-        /// <summary>
-        /// Pokój startowy
-        /// </summary>
-        Start = 2,
-        /// <summary>
-        /// Pokój ze Strażnikiem
-        /// </summary>
-        Boss = 3,
-        /// <summary>
-        /// Pokój z unikalnym przedmiotem
-        /// </summary>
-        Premium = 4,
-        /// <summary>
-        /// Pokój ze sklepem
-        /// </summary>
-        Shop = 5
-
-    }
-
-    /// <summary>
     /// Obiekt klasy Random odpowiadający za losowanie.
     /// </summary>
     private Random _rand;
+
+    private int _minRoomCount = 15;
     /// <summary>
-    /// Minimalna ilość pokoi (domyślnie 15).
+    /// Minimalna ilość pokoi.
     /// </summary>
     public int MinRoomCount
     {
         get
         {
-            return MinRoomCount;
+            return _minRoomCount;
         }
         set
         {
-            value = 15;
+            _minRoomCount = value;
         }
     }
+
+    private int _maxRoomCount = 20;
     /// <summary>
-    /// Maksymalna ilość pokoi (domyślnie 20).
+    /// Maksymalna ilość pokoi.
     /// </summary>
     public int MaxRoomCount
     {
         get
         {
-            return MaxRoomCount;
+            return _maxRoomCount;
         }
         set
         {
-            value = 20;
+            _maxRoomCount = value;
         }
     }
 
+    private int _boardSize;
     /// <summary>
     /// Właściwość klasy Board określająca rozmiar planszy.
     /// </summary>
@@ -125,31 +98,36 @@ public class Board
     {
         get
         {
-            return BoardSize;
+            return _boardSize;
         }
         set
         {
             HalfOfBoardSize = value / 2;
-            BoardSize = value;
+            _boardSize = value;
         }
     }
 
     /// <summary>
     /// Tablica zawierająca informację o rozmieszczeniu pokoi.
     /// </summary>
-    public int[,] BoardTab
+    public Cell[,] BoardTab
     {
         get;
         set;
     }
 
     /// <summary>
-    /// Połowa rozmiaru planszy, ustawiana w konstruktorze klasy <see cref="Board"/>.
+    /// Połowa rozmiaru planszy.
     /// </summary>
     public int HalfOfBoardSize
     {
         get;
         private set;
+    }
+
+    public Cell this[int x, int y]
+    {
+        get { return BoardTab[x, y]; }
     }
 
     /// <summary>
@@ -158,8 +136,9 @@ public class Board
     /// <param name="boardSize">Rozmiar planszy.</param>
     public Board(int boardSize)
     {
-        BoardTab = new int[boardSize, boardSize];
+        BoardTab = new Cell[boardSize, boardSize];
         BoardSize = boardSize;
+        ResetBoard();
     }
 
     /// <summary>
@@ -167,18 +146,18 @@ public class Board
     /// </summary>
     /// <param name="boardSize">Rozmiar planszy.</param>
     /// <param name="board">Czyszczona plansza.</param>
-    public void ResetBoard(int boardSize, int[,] board)
+    public void ResetBoard()
     {
-        for (int i = 0; i < boardSize; i++)
-            for (int j = 0; j < boardSize; j++)
-                board[i, j] = 0;
+        for (int i = 0; i < BoardSize; i++)
+            for (int j = 0; j < BoardSize; j++)
+                BoardTab[i, j] = new Cell(CellType.Empty);
     }
 
     /// <summary>
     /// Główna metoda odpowiedzialna za wygenerowanie rozmieszczenia pokoi.
     /// </summary>
     /// <param name="seed">Ziarno losowości (domyślnie pusty). Jeśli puste generuje nowe ziarno.</param>
-    /// <returns>Ziarno rozmieszczenia pokoi.</returns>
+    /// <returns>Zwraca ziarno rozmieszczenia pokoi.</returns>
     public string FillBoard(string seed = "")
     {
         const int roomOffset = 3;
@@ -194,7 +173,7 @@ public class Board
 
         _rand = new Random(seed.GetHashCode());
 
-        BoardTab[HalfOfBoardSize, HalfOfBoardSize] = (int)Cell.Start;
+        BoardTab[HalfOfBoardSize, HalfOfBoardSize] = new Cell(CellType.Start);
         RandomNeighbors(1);
         MakeCoreRoom(roomOffset, roomCoreCount);
 
@@ -202,31 +181,38 @@ public class Board
     }
 
     /// <summary>
-    /// Metoda pomocnicza (<see cref="FillBoard(string)"/>) odpowiadająca za ilość sąsiadów danej komórki.
+    /// Metoda pomocnicza odpowiadająca za ilość sąsiadów danej komórki.
     /// </summary>
+    /// <remarks>
+    /// Metoda pomocniczna wykorzystywana w metodzie <see cref="FillBoard(string)"./>
+    /// </remarks>
     /// <param name="roomCounter">Liczba pokoi znajdujących się już w tabeli.</param>
     private void RandomNeighbors(int roomCounter)
     {
         bool isRandomized = false;
 
-        int iLowerLimit = 3, jLowerLimit = 3, iUpperLimit = 3, jUpperLimit = 3;
+        int columnLowerLimit = HalfOfBoardSize - 1;
+        int rowLowerLimit = HalfOfBoardSize - 1;
+        int columnUpperLimit  = HalfOfBoardSize - 1;
+        int rowUpperLimit  = HalfOfBoardSize - 1;
+
         do
         {
-            for (int i = iLowerLimit; i < BoardSize - iUpperLimit; i++)
+            for (int column = columnLowerLimit; column < BoardSize - columnUpperLimit; column++)
             {
-                for (int j = jLowerLimit; j < BoardSize - jUpperLimit; j++)
+                for (int row = rowLowerLimit; row < BoardSize - rowUpperLimit; row++)
                 {
-                    if (MakeRoom(i, j))
+                    if (MakeRoom(column, row))
                     {
                         roomCounter++;
-                        if (i == iLowerLimit)
-                            iLowerLimit = iLowerLimit == 0 ? 0 : iLowerLimit - 1;
-                        else if (i == iUpperLimit)
-                            iUpperLimit = iUpperLimit == 0 ? 0 : iUpperLimit - 1;
-                        if (j == jLowerLimit)
-                            jLowerLimit = jLowerLimit == 0 ? 0 : jLowerLimit - 1;
-                        else if (j == jUpperLimit)
-                            jUpperLimit = jUpperLimit == 0 ? 0 : jUpperLimit - 1;
+                        if (column == columnLowerLimit)
+                            columnLowerLimit = columnLowerLimit == 0 ? 0 : columnLowerLimit - 1;
+                        else if (column == columnUpperLimit)
+                            columnUpperLimit = columnUpperLimit == 0 ? 0 : columnUpperLimit - 1;
+                        if (row == rowLowerLimit)
+                            rowLowerLimit = rowLowerLimit == 0 ? 0 : rowLowerLimit - 1;
+                        else if (row == rowUpperLimit)
+                            rowUpperLimit = rowUpperLimit == 0 ? 0 : rowUpperLimit - 1;
                     }
                     if (roomCounter >= MinRoomCount)
                     {
@@ -242,41 +228,41 @@ public class Board
     }
 
     /// <summary>
-    /// Metoda pomocnicza (<see cref="MakeRoom(int, int)"/>) zliczająca sąsiadów danej komórki tabeli.
+    /// Metoda pomocnicza zliczająca sąsiadów danej komórki tabeli.
     /// </summary>
-    /// <param name="x">Indeks wiersza tabeli.</param>
-    /// <param name="y">Indeks kolumny tabeli.</param>
+    /// <param name="x">Indeks kolumny tabeli.</param>
+    /// <param name="y">Indeks wiersza tabeli.</param>
     /// <param name="hasCloseNeighbours">Parametr typu out zwracający nam informację o tym czy dana komórka ma sąsiada w kierunku N/E/S/W.</param>
     /// <param name="checkOnlyCloseNeighbours">Parametr domyślny (false) decydujący o tym czy mają być sprawdzani wszyscy sąsiedzi wokół komórki czy tylko w podstawowych kierunkach N/E/S/W.</param>
-    /// <returns>Ilość sąsiadów</returns>
+    /// <returns>Metoda zwraca ilość sąsiadów</returns>
     private int NumberOfNeighbors(int x, int y, out bool hasCloseNeighbours, bool checkOnlyCloseNeighbours = false)
     {
         hasCloseNeighbours = false;
         int numOfNeighbors = 0;
 
         if (y - 1 >= 0)
-            if (BoardTab[x, y - 1] != (int)Cell.Empty)
+            if (BoardTab[x, y - 1].Type != CellType.Empty)
             {
                 numOfNeighbors++;
                 hasCloseNeighbours = true;
             }
 
         if (x + 1 < BoardSize)
-            if (BoardTab[x + 1, y] != (int)Cell.Empty)
+            if (BoardTab[x + 1, y].Type != CellType.Empty)
             {
                 numOfNeighbors++;
                 hasCloseNeighbours = true;
             }
 
         if (y + 1 < BoardSize)
-            if (BoardTab[x, y + 1] != (int)Cell.Empty)
+            if (BoardTab[x, y + 1].Type != CellType.Empty)
             {
                 numOfNeighbors++;
                 hasCloseNeighbours = true;
             }
 
         if (x - 1 >= 0)
-            if (BoardTab[x - 1, y] != (int)Cell.Empty)
+            if (BoardTab[x - 1, y].Type != CellType.Empty)
             {
                 numOfNeighbors++;
                 hasCloseNeighbours = true;
@@ -285,19 +271,19 @@ public class Board
         if (!checkOnlyCloseNeighbours)
         {
             if (x - 1 >= 0 && y - 1 >= 0)
-                if (BoardTab[x - 1, y - 1] != (int)Cell.Empty)
+                if (BoardTab[x - 1, y - 1].Type != CellType.Empty)
                     numOfNeighbors++;
 
             if (x + 1 < BoardSize && y - 1 >= 0)
-                if (BoardTab[x + 1, y - 1] != (int)Cell.Empty)
+                if (BoardTab[x + 1, y - 1].Type != CellType.Empty)
                     numOfNeighbors++;
 
             if (x + 1 < BoardSize && y + 1 < BoardSize)
-                if (BoardTab[x + 1, y + 1] != (int)Cell.Empty)
+                if (BoardTab[x + 1, y + 1].Type != CellType.Empty)
                     numOfNeighbors++;
 
             if (x - 1 >= 0 && y + 1 < BoardSize)
-                if (BoardTab[x - 1, y + 1] != (int)Cell.Empty)
+                if (BoardTab[x - 1, y + 1].Type != CellType.Empty)
                     numOfNeighbors++;
         }
 
@@ -305,11 +291,11 @@ public class Board
     }
 
     /// <summary>
-    /// Metoda pomocnicza (<see cref="RandomNeighbors(int)"/>) tworząca pokój na podstawie prawdopodobieństwa i ilości sąsiadów dla danego pola.
+    /// Metoda pomocnicza tworząca pokój na podstawie prawdopodobieństwa i ilości sąsiadów dla danego pola.
     /// </summary>
-    /// <param name="x">Indeks wiersza tabeli.</param>
-    /// <param name="y">Indeks kolumny tabeli.</param>
-    /// <returns>Informacja czy pokój został stworzony.</returns>
+    /// <param name="x">Indeks kolumny tabeli.</param>
+    /// <param name="y">Indeks wiersza tabeli.</param>
+    /// <returns>Metoda zwraca informację czy pokój został stworzony.</returns>
     private bool MakeRoom(int x, int y)
     {
         bool hasCloseNeighbours;
@@ -317,7 +303,7 @@ public class Board
         var percent = _rand.Next(0, 101);
         bool isMaked = false;
 
-        if (BoardTab[x, y] != (int)Cell.Empty)
+        if (BoardTab[x, y].Type != CellType.Empty)
             return isMaked;
 
         if (!hasCloseNeighbours)
@@ -330,21 +316,21 @@ public class Board
             case 1:
                 if (percent <= 50)
                 {
-                    BoardTab[x, y] = (int)Cell.Common;
+                    PlaceRoom(x, y, CellType.Common);
                     isMaked = true;
                 }
                 break;
             case 2:
                 if (percent <= 40)
                 {
-                    BoardTab[x, y] = (int)Cell.Common;
+                    PlaceRoom(x, y, CellType.Common);
                     isMaked = true;
                 }
                 break;
             case 3:
                 if (percent <= 10)
                 {
-                    BoardTab[x, y] = (int)Cell.Common;
+                    PlaceRoom(x, y, CellType.Common);
                     isMaked = true;
                 }
                 break;
@@ -363,9 +349,9 @@ public class Board
     }
 
     /// <summary>
-    /// Metoda pomocnicza (<see cref="FillBoard(string)"/>) tworząca unikalne pokoje.
+    /// Metoda tworząca unikalne pokoje.
     /// </summary>
-    /// <param name="roomOffset">Indeks w <see cref="Cell"/> pierwszego z unikalnych pokoi.</param>
+    /// <param name="roomOffset">Indeks w <see cref="CellType"/> pierwszego z unikalnych pokoi.</param>
     /// <param name="roomCoreCount">Ilość unikalnych pokoi.</param>
     private void MakeCoreRoom(int roomOffset, int roomCoreCount)
     {
@@ -378,7 +364,7 @@ public class Board
         for (int i = 0; i < BoardSize; i++)
             for (int j = 0; j < BoardSize; j++)
             {
-                if (BoardTab[i, j] == (int)Cell.Common)
+                if (BoardTab[i, j].Type == CellType.Common)
                 {
                     numOfNeighbours = NumberOfNeighbors(i, j, out trash, true);
                     if (numOfNeighbours == 1)
@@ -394,19 +380,19 @@ public class Board
                 index = _rand.Next(0, singleRoomList.Count);
                 cell = singleRoomList[index];
                 singleRoomList.RemoveAt(index);
-                BoardTab[cell.X, cell.Y] = i + roomOffset;
+                PlaceRoom(cell.X, cell.Y, (CellType)(i + roomOffset));
             }
         else
         {
             for (int i = 0; i < BoardSize; i++)
                 for (int j = 0; j < BoardSize; j++)
                 {
-                    if (BoardTab[i, j] == (int)Cell.Empty)
+                    if (BoardTab[i, j].Type == CellType.Empty)
                     {
                         numOfNeighbours = NumberOfNeighbors(i, j, out trash, true);
                         if (numOfNeighbours == 1)
                         {
-                            BoardTab[i, j] = (int)Cell.Boss;
+                            PlaceRoom(i, j, CellType.Boss);
                             //Wychodzimy z obu pętli
                             i = BoardSize;
                             break;
@@ -416,4 +402,36 @@ public class Board
         }
     }
 
+    /// <summary>
+    /// Metoda do umieszczania komórki w tabeli z aktualizacją zmiennych dla sąsiednich komórek.
+    /// </summary>
+    /// <param name="x">Indeks kolumny tabeli.</param>
+    /// <param name="y">Indeks wiersza tabeli.</param>
+    /// <param name="type"></param>
+    public void PlaceRoom(int x, int y, CellType type)
+    {
+        BoardTab[x, y].Type = type;
+
+        if (y - 1 >= 0)
+        {
+            BoardTab[x, y].BottomNeighbour = BoardTab[x, y - 1].Type;
+            BoardTab[x, y - 1].TopNeighbour = type;
+        }
+        if (y + 1 < BoardSize)
+        {
+            BoardTab[x, y].TopNeighbour = BoardTab[x, y + 1].Type;
+            BoardTab[x, y + 1].BottomNeighbour = type;
+        }
+
+        if (x + 1 < BoardSize)
+        {
+            BoardTab[x, y].RightNeighbour = BoardTab[x + 1, y].Type;
+            BoardTab[x + 1, y].LeftNeighbour = type;
+        }
+        if (x - 1 >= 0)
+        {
+            BoardTab[x, y].LeftNeighbour = BoardTab[x - 1, y].Type;
+            BoardTab[x - 1, y].RightNeighbour = type;
+        }
+    }
 }
