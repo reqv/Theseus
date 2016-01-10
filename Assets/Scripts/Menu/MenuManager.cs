3b1c6e3418427b0f56689689a7567461d330e395
Lukasz Rabiec <lukaszrabiec@gmail.com>
2016-01-10 18:19:39 +0100

    Menu

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+
+    public void NewGame()
+    {
+        Application.LoadLevel("Main");
+    }
+
+    public void Authors()
+    {
+        Application.LoadLevel("Authors");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
