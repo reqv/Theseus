cd14202db07773f3049063276cb188957eb009ee
Jakub Majewski <dzejkopp@gmail.com>
2016-01-11 01:58:29 +0100

    Minotaura ciąg dalszy + dodane Sprite pochodni i mocy

--- Assets/Scripts/Monsters/Minotaur.cs
+++ Assets/Scripts/Monsters/Minotaur.cs
@@ -12,18 +12,29 @@ public class Minotaur : Monster
         Minions
     }
 
+    #region
+    [SerializeField]
+    private Projectile[] _magicProjectiles;
+    #endregion
     private MinotaurAction _actualAction;
     private GameObject _player;
 
-    private Vector3 _meleeOffset = new Vector3(0.4f, 0.35f, 0);
+    private Vector3 _meleeOffsetR = new Vector3(0.4f, 0.35f, 0);
+    private Vector3 _meleeOffsetL = new Vector3(-0.4f, 0.35f, 0);
     private bool _readyToAttack = false;
     private bool _meleePerformed = false;
-    private float _chaseTimer = 0;
+    private float _actionTimer = 0;
+
+    private float _magicDelayTimer = 0;
+    private bool _magicDirecion;
+    [SerializeField]
+    private float _magicDelay;
 
 	void Start () 
     {
         base.Start();
         _player = GameObject.FindGameObjectWithTag("Player");
+        _targetToAttack = _player.transform;
 	}
 
     public override void FixedUpdate()
@@ -37,10 +48,8 @@ public class Minotaur : Monster
 
         if(_actualAction == MinotaurAction.Idle)
         {
-            _actualAction = (MinotaurAction)RandomNumber(1, 4);
-
-            if (_actualAction == MinotaurAction.Melee)
-                _chaseTimer = 0;
+            _actualAction = (MinotaurAction)RandomNumber(1, 2);
+                _actionTimer = 0;
         }
 
         switch(_actualAction)
@@ -49,6 +58,7 @@ public class Minotaur : Monster
                 Chase();
                 break;
             case MinotaurAction.Magic:
+                MagicAttack();
                 break;
             case MinotaurAction.Rocks:
                 break;
@@ -59,9 +69,20 @@ public class Minotaur : Monster
 
     private void UpdateTimers()
     {
-        _chaseTimer += Time.deltaTime;
+        _actionTimer += Time.deltaTime;
+        _magicDelayTimer += Time.deltaTime;
     }
 
+    public override void Walking()
+    {
+    }
+
+    public override void SetCrowdControl(Status status, float time)
+    {
+        return;
+    }
+
+    #region Action Melee
     public override void Attack()
     {
         _Anim.SetBool("Attacking", true);
@@ -79,8 +100,7 @@ public class Minotaur : Monster
 
     public override void Chase()
     {
-        Debug.Log(_chaseTimer);
-        if(_chaseTimer > 7.0f)
+        if(_actionTimer > 7.0f)
         {
             _actualAction = MinotaurAction.Idle;
             _Anim.SetBool("Walking", false);
@@ -94,17 +114,16 @@ public class Minotaur : Monster
 
         if (_player != null)
         {
-            _targetToAttack = _player.transform;
-            distanceR = WhereIsATarget(_targetToAttack.position + _meleeOffset, true);
-            distanceL = WhereIsATarget(_targetToAttack.position - _meleeOffset, true);
+            distanceR = WhereIsATarget(_targetToAttack.position + _meleeOffsetR, true);
+            distanceL = WhereIsATarget(_targetToAttack.position + _meleeOffsetL, true);
             if (distanceL < distanceR)
             {
-                WhereIsATarget2(_targetToAttack.position - _meleeOffset);
+                WhereIsATarget2(_targetToAttack.position + _meleeOffsetL);
                 distance = distanceL;
             }
             else
             {
-                WhereIsATarget2(_targetToAttack.position - _meleeOffset);
+                WhereIsATarget2(_targetToAttack.position + _meleeOffsetR);
                 distance = distanceR;
             }
         }
@@ -136,18 +155,9 @@ public class Minotaur : Monster
             Flip();
     }
 
-    public override void Walking()
-    {
-    }
-
-    public override void SetCrowdControl(Status status, float time)
-    {
-        return;
-    }
-
     void OnTriggerStay2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
         {
             _readyToAttack = true;
             if (_meleePerformed)
@@ -169,4 +179,43 @@ public class Minotaur : Monster
 
         _axis.Normalize();
     }
+    #endregion
+
+    #region Action Magic
+    private void MagicAttack()
+    {
+        if (_actionTimer > 4)
+        {
+            _actualAction = MinotaurAction.Idle;
+        }
+
+        if (_magicDelayTimer > _magicDelay)
+        {
+            _magicDelayTimer = 0;
+            if (_magicDirecion)
+            {
+                NewProjectile2(_magicProjectiles[0].gameObject, new Vector2(0, 0.2f), Vector2.up * _bulletVelocityFactor);
+                NewProjectile2(_magicProjectiles[1].gameObject, new Vector2(0.2f, 0), Vector2.right * _bulletVelocityFactor);
+                NewProjectile2(_magicProjectiles[2].gameObject, new Vector2(-0.2f, 0), Vector2.left * _bulletVelocityFactor);
+                NewProjectile2(_magicProjectiles[3].gameObject, new Vector2(0, -0.2f), Vector2.down * _bulletVelocityFactor);
+            }
+            else
+            {
+                NewProjectile2(_magicProjectiles[0].gameObject, new Vector2(0, 0.2f), (Vector2.up + Vector2.right) * _bulletVelocityFactor);
+                NewProjectile2(_magicProjectiles[1].gameObject, new Vector2(0.2f, 0), (Vector2.down + Vector2.right) * _bulletVelocityFactor);
+                NewProjectile2(_magicProjectiles[2].gameObject, new Vector2(-0.2f, 0), (Vector2.up + Vector2.left) * _bulletVelocityFactor);
+                NewProjectile2(_magicProjectiles[3].gameObject, new Vector2(0, -0.2f), (Vector2.down + Vector2.left) * _bulletVelocityFactor);
+            }
+
+            _magicDirecion = !_magicDirecion;
+        }
+    }
+
+    protected void NewProjectile2(GameObject projectile, Vector2 offset, Vector2 velocity, float rotate = 0)
+    {
+        var bullet = Instantiate(projectile, new Vector2(this.transform.position.x + offset.x, this.transform.position.y + offset.y), Quaternion.Euler(new Vector3(0, 0, rotate))) as GameObject;
+        bullet.GetComponent<Rigidbody2D>().velocity = velocity;
+        bullet.GetComponent<Projectile>().Damage = _attackPower;
+    }
+    #endregion
 }
